cde989f2b1aaac1dfd22f63b5d57aa092abcfc88
Alexandr Savca <alexandr.savca89@gmail.com>
2023-07-08 08:18:04 +0300

    utils.mk: urlcodes -> deadlinks, add fake url to exception

--- utils.mk
+++ utils.mk
@@ -1,10 +1,11 @@
 GREPOPT = --exclude-dir=.git --exclude-dir=.github --exclude-dir=font \
 	  --exclude-dir=images -R .
 
-urlcodes:
-	@echo "=======> Check URLs for non-200 response code"
+deadlinks:
+	@echo "=======> Check for dead links"
 	@grep -Eiho "https?://[^\"\\'>< ]+" ${GREPOPT}  \
+		| grep -Ev "https?://www.gnu.org/software/somelib/index.html" \
 		| grep -v fileserver.intranet           \
 		| httpx -silent -fc 200 -sc
 
-.PHONY: urlcodes
+.PHONY: deadlinks
